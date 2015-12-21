23f9f044dba805138ae0c7184d0b476f1109b722
Richard Wilde <richard@wildesoft.net>
2015-12-21 07:11:23 +0000

    Using BindingEngine for list changes  (not observerLocator)

--- src/welcome.js
+++ src/welcome.js
@@ -1,9 +1,5 @@
-/*
-Would like to change the observerLocator to the BindingEngin but initially I couldnlt get it to work
-http://stackoverflow.com/questions/30283569/array-subscription-in-aurelia/30286225#comment56507826_30286225
-*/
 
-import {ObserverLocator, inject} from 'aurelia-framework';
+import {BindingEngine, inject} from 'aurelia-framework';
 //import {HttpClient} from 'aurelia-fetch-client';
 //import 'fetch';
 import {Storage} from './helpers/storage';
@@ -14,118 +10,95 @@ import $ from 'jquery';
 
 
 //@inject(HttpClient, Storage, Swag, ObserverLocator, BaseConfig)
-@inject(Swag, ObserverLocator, BaseConfig, Clone)
+@inject(Swag, BindingEngine, BaseConfig, Clone)
 
 export class Test {
 
-  //constructor(http, storage, swag, observerLocator, config) {
-  constructor(swag, observerLocator, config, clone) {
+    //constructor(http, storage) {
+    constructor(swag, bindingEngine, config, clone) {
+
+        this.swag = swag;
+        this.clone = clone;
+
+        //this.storage.set("test", "rippo");
+
+        this.heading = 'Tonights Attendees';
+
+        this.undoAttendeeEnabled = false;
+        this.swagEnabled = true;
+        this.currentWinner = null;
+        this.winnerList = [];
+        this.attendeeList = config.current.tempAttendees;
+        this.undoAttendeeList = [];
+
+
+        bindingEngine
+            .collectionObserver(this.attendeeList)
+            .subscribe(splices => this.attendeeListRemove(splices));
+            
+        // http.configure(config => {
+        //   config
+        //     .useStandardConfiguration()
+        //     .withBaseUrl('http://localhost:6648/');
+        // });
+    }
+
+    attendeeListRemove(splices) {
+        //TODO Need to check actually if we have a removed splice!
+        
+        //Get removed item
+        var item = this.clone.copy(splices[0].removed[0]);
+        //Get random piece of swag
+        item.swagThing = this.swag.randomThing();
+
+        //push to winner list
+        this.winnerList.push(item);
+
+        //Should swag button be disabled?
+        this.swagEnabled = (this.attendeeList.length > 0) && (this.swag.countUnwon() > 0);
+    }
+
+    activate() {
+        // return this.http.fetch('home/memberlist')
+        //   .then(response => response.json())
+        //   .then(users => this.attendeeList = users);
+    }
+
+    removeAttendee(user) {
+        if (this.undoAttendeeList.length === 0)
+            this.undoAttendeeEnabled = true;
+
+        this.undoAttendeeList.push(user);
+        this.attendeeList = this.attendeeList.filter(function (item) {
+            return (item !== user);
+        });
+    }
+
+    removeWinner(user) {
+        this.winnerList = this.winnerList.filter(function (item) {
+            return (item !== user);
+        });
+
+        this.attendeeList.push(user);
+        this.swagEnabled = true;
+    }
+
+
+    undohandler() {
+        var user = this.undoAttendeeList.slice(-1).pop();
+        this.undoAttendeeList.pop();
+        this.attendeeList.push(user);
+
+        if (this.undoAttendeeList.length === 0)
+            this.undoAttendeeEnabled = false;
+    }
     
-    //this.http = http;
-    //this.storage = storage;
-    this.swag = swag;
-    this.clone = clone;
+    //Selects random winner
+    random() {
     
-    //this.storage.set("test", "rippo");
- 
-    
-    this.heading = 'Tonights Attendees';
+        //get random memberId from attendee list    
+        var random = Math.floor(Math.random() * $('#attendeeList .card').length);
+        this.attendeeList.splice(random, 1);
 
-    this.undoAttendeeEnabled = false;
-    this.swagEnabled = true;
-    this.currentWinner = null;
-    this.winnerList = [];
-    this.attendeeList = config.current.tempAttendees;
-    this.undoAttendeeList = [];
-
-    // subscribe
-    
-    observerLocator
-      .getArrayObserver(this.attendeeList)
-      .subscribe(splices => {
-          //Get removed item
-          var item = this.clone.copy(splices[0].removed[0]);
-          //Get random piece of swag
-          item.swagThing = this.swag.randomThing();
-
-          //push to winner list
-          this.winnerList.push(item);
-
-          //Should swag button be disabled?
-          this.swagEnabled = (this.attendeeList.length > 0) && (this.swag.countUnwon() > 0);
-
-      });
-
-//  
-      // let subscription = bindingEngine.collectionObserver(this.attendeeList)
-      //.subscribe(splices => console.log(splices));
-
-    // unsubscribe
-    //subscription.dispose();
- 
-//     var subscription = bindingEngine.collectionObserver(this.attendeeList)
-//       .subscribe(
-//           splices =>  {
-//               console.log(clone);
-//               console.log(splices);
-//           }
-//       );
-// 
-//     // unsubscribe
-//     subscription.dispose();    
-
-
-
-    // http.configure(config => {
-    //   config
-    //     .useStandardConfiguration()
-    //     .withBaseUrl('http://localhost:6648/');
-    // });
-  }
-
-
-  activate() {
-    // return this.http.fetch('home/memberlist')
-    //   .then(response => response.json())
-    //   .then(users => this.attendeeList = users);
-  }
-
-
-  removeAttendee(user) {
-    if (this.undoAttendeeList.length === 0)
-      this.undoAttendeeEnabled = true;
-    
-    this.undoAttendeeList.push(user);
-    this.attendeeList = this.attendeeList.filter(function(item) {
-      return (item !== user);
-    });
-  }
-
-  removeWinner(user) {
-    this.winnerList = this.winnerList.filter(function(item) {
-      return (item !== user);
-    });
-
-    this.attendeeList.push(user);
-    this.swagEnabled = true;
-  }
- 
-
-  undohandler() {
-    var user = this.undoAttendeeList.slice(-1).pop();
-    this.undoAttendeeList.pop();
-    this.attendeeList.push(user);
-    
-    if (this.undoAttendeeList.length === 0)
-      this.undoAttendeeEnabled = false;
-  }
-    
-  //Selects random winner
-  random() {
-    
-    //get random memberId from attendee list    
-    var random = Math.floor(Math.random() * $('#attendeeList .card').length);
-    this.attendeeList.splice(random, 1);
- 
-  }
+    }
 };
